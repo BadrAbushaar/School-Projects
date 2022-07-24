da333e7e68cc9be37e5cff5aad4ced56e728a09f
Badr <85860504+BadrAbushaar@users.noreply.github.com>
2022-07-23 20:05:56 -0400

    Update SortedList.java
    add abstract class and comments describing methods

--- SortedList.java
+++ SortedList.java
@@ -1,8 +1,39 @@
+/*Recursive implementation of a sorted linked list that implements the following methods:
+    - insert
+    - remove
+    - search
+    - retrieve
+    - iterator 
+*/
+
 import java.util.Iterator;
+
+public abstract class List<E> implements Iterable<E> {
+    protected Node<E> head;
+
+    protected class Node<T> {
+        protected T data;
+        protected Node<T> next;
+
+        protected Node(T data) {
+            this.data = data;
+        }
+
+    }
+
+    public abstract void insert(E data);
+
+    public abstract void remove(E data);
+
+    public abstract E retrieve(int index);
+
+    public abstract boolean search(E data);
+}
 
 public class SortedList<E extends Comparable<? super E>> extends List<E> {
 
     // Insert method
+    // insert a node into the linked list
     public void insert(E data) {
         Node<E> curr = head;
         head = insertHelper(data, curr);
@@ -10,14 +41,14 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
 
     // Insert helper method
     public Node<E> insertHelper(E data, Node<E> curr) {
-        // Empty list
+        // if LinkedList is empty
         if (curr == null) {
             Node<E> temp = new Node<E>(data);
             curr = temp;
             temp.next = null;
             return curr;
         }
-
+        // if data of new node is less than current head
         if (data.compareTo(curr.data) < 0) {
             Node<E> temp = new Node<E>(data);
             temp.next = curr;
@@ -30,6 +61,7 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
     }
 
     // Remove method
+    // remove a node from the LinekdList
     public void remove(E data) {
         Node<E> curr = head;
         head = removeHelper(data, curr);
@@ -37,10 +69,10 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
 
     // Remove helper method
     public Node<E> removeHelper(E data, Node<E> curr) {
+        // if LinkedList is empty
         if (curr == null) {
             return null;
         }
-
         if (data.compareTo(curr.data) == 0) {
             curr = curr.next;
             return curr;
@@ -51,6 +83,7 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
     }
 
     // Retrieve method
+    // retrieve the data from a specified node
     public E retrieve(int index) {
         Node<E> curr = head;
         int count = 0;
@@ -59,10 +92,10 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
 
     // Retrieve helper method
     public E retrieveHelper(int index, Node<E> curr, int count) {
+        // if LinkedList is empty
         if (curr == null) {
             return null;
         }
-
         if (index == count) {
             return curr.data;
         } else {
@@ -71,6 +104,7 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
     }
 
     // Search method
+    // determine if the data is within the LinkedList
     public boolean search(E data) {
         Node<E> curr = head;
         return searchHelper(data, curr);
@@ -78,10 +112,11 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
 
     // Search helper method
     public boolean searchHelper(E data, Node<E> curr) {
+        // if LinkedList is empty
         if (curr == null) {
             return false;
         }
-
+        
         if (data.compareTo(curr.data) == 0) {
             return true;
         } else {
